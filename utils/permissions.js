1ecc4cdecda91393b27c50f72d448dcda9681908
Guillaume Hélouis <1146421+ghelouis@users.noreply.github.com>
2020-10-11 22:50:18 +0200

    Remove unused file

--- utils/permissions.js
+++
@@ -1,9 +0,0 @@
-import * as Permissions from "expo-permissions";
-import i18n from "i18n-js";
-
-export async function getCameraPermissions() {
-    const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
-    if (status !== 'granted') {
-        alert(i18n.t('errors.missingPermission'));
-    }
-}
